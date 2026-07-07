fb39bfe69e2718a833402b8b005acc6a7ea6ff5f
Denis-pyth <desirefeyisola2@gmail.com>
2026-07-07 20:30:48 +0100

    feat: implement VA expiry, requery fallback, and CSV statements

--- backend/src/group/group.module.ts
+++ backend/src/group/group.module.ts
@@ -5,11 +5,14 @@ import { GroupService } from './group.service';
 import { AjoGroup } from '../entities/ajo-group.entity';
 import { GroupMember } from '../entities/group-member.entity';
 import { User } from '../entities/user.entity';
+import { Transaction } from '../entities/transaction.entity'; // <-- NEW IMPORT
 import { NombaModule } from '../nomba/nomba.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AjoGroup, GroupMember, User]),
-   NombaModule],
+  imports: [
+    TypeOrmModule.forFeature([AjoGroup, GroupMember, User, Transaction]), // <-- ADDED Transaction
+    NombaModule
+  ],
   controllers: [GroupController],
   providers: [GroupService],
 })
